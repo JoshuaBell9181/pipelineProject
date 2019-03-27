6a6b77ccdb515e4e0b12d834b7ab8ee05309693d
Joshua <31083115+JoshuaBell9181@users.noreply.github.com>
2019-03-26 21:58:27 -0300

    updated controller but addition not working

--- SimpleCompArch2019/controller.vhd
+++ SimpleCompArch2019/controller.vhd
@@ -41,10 +41,8 @@ architecture fsm of controller is
 
   type state_type is (S0,S1,S1a,S1b,S1c,S2,S2a,S2b,S2c,S3Delay,S3,S3a,S3b,S4Delay,S4,S4a,S4b,S5Delay,S5,S5a,S5b,S6Delay,S6,S6a,S6b,S7Delay,S7,S7a,S7b,S8Delay,
 							S8,S8a,S8b,S9Delay,S9,S9a,S9b,S10,S11Delay,S11,S11a,S11b,S12Delay,S12,S12a,S12b);
+							
   signal state: state_type;
-  signal delaystate: state_type;
-  constant memdelay: integer :=1;
-  signal usedelay: boolean := false;
   signal IR_PS2: std_logic_vector(15 downto 0);
   signal IR_PS3: std_logic_vector(15 downto 0);
 
@@ -53,26 +51,28 @@ architecture fsm of controller is
   procedure stage1 is -- Fetch Instruction, enable memread, set address mux to pass addrr. on PC
 	begin
 		PCinc_ctrl <= '0';	
-		IRld_ctrl <= '1'; 	
-		Mre_ctrl <= '1';  	
+		IRld_ctrl <= '1'; 	  	
 		Ms_ctrl <= "10";  	  
 		RFwe_ctrl <= '0'; 
 		RFr1e_ctrl <= '0'; 
 		RFr2e_ctrl <= '0'; 
 		Mwe_ctrl <= '0';
 		oe_ctrl <= '0';
+		jmpen_ctrl <= '0';
 		immdata_ctrl <= '0';
   end procedure stage1;
- 
-  --stage1a is a delay	
   
-  procedure stage1b is
+	
+	--stage1a is a delay	
+	
+	procedure stage1b is
    begin
 		IRld_ctrl <= '0';
 		PCinc_ctrl <= '1';
-		Mre_ctrl <= '0';
+		Mwe_ctrl <= '0';
 	end procedure stage1b;
 		
+
   function stage2(IR_word: std_logic_vector(15 downto 0)) return state_type is  -- Decode inst, disable increment of PC
     variable OPCODE_S1: std_logic_vector(3 downto 0);
     variable next_state: state_type;
@@ -86,7 +86,7 @@ architecture fsm of controller is
 		when mov4 		=> 	next_state := S6Delay;
 		when add 		=>  next_state := S7Delay;
 		when subt 		=>	next_state := S8Delay;
-		--when jz 		=>	next_state := S27;
+		when jz 		=>	next_state := S9Delay;
 		when halt 		=>	next_state := S10; 
 		when readm 		=> 	next_state := S11Delay;   
 		when mov5 => 	next_state := S12Delay;
@@ -105,8 +105,8 @@ begin
 		IRld_ctrl <= '0';
 		RFs_ctrl <= "00";		
 		Rfwe_ctrl <= '0';
+		Mwe_ctrl <= '0';
 		Mre_ctrl <= '0';
-		Mwe_ctrl <= '0';					
 		jmpen_ctrl <= '0';
 		immdata_ctrl <= '0';		
 		oe_ctrl <= '0';
@@ -120,43 +120,44 @@ begin
 			
 				
 	  when S1 => 
-			stage1;
+			stage1; -- Fetch Instruction, enable memread, set address mux to pass addrr. on PC
 			state <= S1a;
-	  when S1a => 	--satge1a is a delay 
+	  when S1a => 	 
 			state <= S1b;  	  	
-	  when S1b => 	
+	  when S1b => 	--satge1b is a delay
 			stage1b;
+			state <= S1c;
 	  when S1c =>
 			PCinc_ctrl <= '0';
 			IR_PS2 <= IR_word;
-			state <= S2;
+			state<=stage2(IR_word);
 	
-	  when S2 =>
-			 stage1;
-			 state <= S2a;
-	  when S2a =>
-			 state <= S2b;
-	  when S2b =>
-			 stage1b;
-			 state <= S2c;
-	  when S2c =>
-			 state<=stage2(IR_word);
+--	  when S2 =>
+--			 stage1;
+--			 state <= S2a;
+--	  when S2a =>
+--			 stage1a;
+--			 state <= S2b;
+--	  when S2b =>
+--			 state <= S2c;
+--	  when S2c =>
+--			 state<=stage2(IR_word);
 			
 	   -- called from stage2
 	  when S3Delay =>
 			 stage1;
 			 state <= S3;
-	  when S3 =>	RFwa_ctrl <= IR_word(11 downto 8);	
+	  when S3 =>	
+	      RFwa_ctrl <= IR_PS2(11 downto 8);	
 			RFs_ctrl <= "01";  -- RF[rn] <= mem[direct]
 			Ms_ctrl <= "01";
-			Mre_ctrl <= '1';
 			Mwe_ctrl <= '0';		  
 	  when S3a => 
 			stage1b;
 			RFwe_ctrl <= '1'; 
-	      Mre_ctrl <= '0'; 
 			state <= S3b;
 	  when S3b => 	
+	       PCinc_ctrl <= '0';
 			 RFwe_ctrl <= '0';
 			 IR_PS2 <= IR_word;
 			 state<=stage2(IR_word);		
@@ -164,17 +165,18 @@ begin
 	  when S4Delay =>
 			 stage1;
 			 state <= S4;
-	  when S4 =>	RFr1a_ctrl <= IR_word(11 downto 8);	
+	  when S4 =>	
+	      RFr1a_ctrl <= IR_PS2(11 downto 8);	
 			RFr1e_ctrl <= '1'; -- mem[direct] <= RF[rn]			
 			Ms_ctrl <= "01";
 			ALUs_ctrl <= "00";	  
 			IRld_ctrl <= '0';
 			state <= S4a;			-- read value from RF
-	  when S4a =>   
-			stage1b;
-			Mre_ctrl <= '0';
+	  when S4a => 
+	      stage1b;  
 			Mwe_ctrl <= '1';		-- write into memory							
-	  when S4b =>   
+	  when S4b => 
+	      PCinc_ctrl <= '0';  
 			Ms_ctrl <= "10";				  
 			Mwe_ctrl <= '0';
 			IR_PS2 <= IR_word;
@@ -183,18 +185,20 @@ begin
 	  when S5Delay =>
 			stage1;
 			state <= S5;
-	  when S5 =>	RFr1a_ctrl <= IR_word(11 downto 8);	
+	  when S5 =>	
+	      RFr1a_ctrl <= IR_PS2(11 downto 8);	
 			RFr1e_ctrl <= '1'; -- mem[RF[rn]] <= RF[rm]
 			Ms_ctrl <= "00";
 			ALUs_ctrl <= "01";
-			RFr2a_ctrl <= IR_word(7 downto 4); 
+			RFr2a_ctrl <= IR_PS2(7 downto 4); 
 			RFr2e_ctrl <= '1'; -- set addr.& data
 			state <= S5a;
-	  when S5a =>   
-			stage1b;
-			Mre_ctrl <= '0';			
+	  when S5a => 
+	      stage1b;  			
 			Mwe_ctrl <= '1'; -- write into memory			
-	  when S5b => 	Ms_ctrl <= "10";-- return
+	  when S5b => 
+	      Ms_ctrl <= "10";-- return
+			PCinc_ctrl <= '0';
 			Mwe_ctrl <= '0';
 			IR_PS2 <= IR_word;
 			state<=stage2(IR_word);
@@ -202,45 +206,52 @@ begin
 	  when S6Delay =>
 			stage1;
 			state <= S6;
-	  when S6 =>	RFwa_ctrl <= IR_word(11 downto 8);	
+	  when S6 =>	
+	      RFwa_ctrl <= IR_PS2(11 downto 8);	
 			RFwe_ctrl <= '1'; -- RF[rn] <= imm.
 			RFs_ctrl <= "10";
 			IRld_ctrl <= '0';
 			state <= S6a;
 	  when S6a =>
-			stage1b;
+			RFwe_ctrl <= '0';
+	      stage1b;
 			state <= S6b;
 	  when S6b =>
+	      PCinc_ctrl <= '0';
 			IR_PS2 <= IR_word; 
 			state<=stage2(IR_word);
 	  
 	  when S7Delay =>
 	      stage1;
 			state <= S7;  
-	  when S7 =>	RFr1a_ctrl <= IR_word(11 downto 8);	
+	  when S7 =>	
+	      RFr1a_ctrl <= IR_PS2(11 downto 8);	
 			RFr1e_ctrl <= '1'; -- RF[rn] <= RF[rn] + RF[rm]
 			RFr2e_ctrl <= '1'; 
-			RFr2a_ctrl <= IR_word(7 downto 4);
+			RFr2a_ctrl <= IR_PS2(7 downto 4);
  			ALUs_ctrl <= "10";
 			state <= S7a;
 	  when S7a =>  
-			stage1b;
+	      stage1b;
 			RFr1e_ctrl <= '0';
 			RFr2e_ctrl <= '0';
 			RFs_ctrl <= "00";
-			RFwa_ctrl <= IR_word(11 downto 8);
+			RFwa_ctrl <= IR_PS2(11 downto 8);
 			RFwe_ctrl <= '1';
 			state <= S7b;
-	  when S7b =>   
+	  when S7b =>  
+			RFwe_ctrl <= '0';
+	      PCinc_ctrl <= '0';
 			IR_PS2 <= IR_word;
 			state<=stage2(IR_word);
 			
 	  when S8Delay =>
 	      stage1;
 			state <= S8;
-	  when S8 =>	RFr1a_ctrl <= IR_word(11 downto 8);	
+	  when S8 =>	
+	      RFr1a_ctrl <= IR_PS2(11 downto 8);	
 			RFr1e_ctrl <= '1'; -- RF[rn] <= RF[rn] - RF[rm]
-			RFr2a_ctrl <= IR_word(7 downto 4);
+			RFr2a_ctrl <= IR_PS2(7 downto 4);
 			RFr2e_ctrl <= '1';  
 			ALUs_ctrl <= "11";
 			state <= S8a;
@@ -249,10 +260,11 @@ begin
 			RFr1e_ctrl <= '0';
 			RFr2e_ctrl <= '0';
 			RFs_ctrl <= "00";
-			RFwa_ctrl <= IR_word(11 downto 8);
+			RFwa_ctrl <= IR_PS2(11 downto 8);
 			RFwe_ctrl <= '1';
 			state <= S8b;
-	  when S8b =>   
+	  when S8b =>  
+	      PCinc_ctrl <= '0'; 
 			IR_PS2 <= IR_word;
 			state<=stage2(IR_word);
 	  
@@ -260,7 +272,7 @@ begin
 			stage1;
 			state <= S9;
 	  when S9 =>
-			stage1b;
+	      stage1b;
 			jmpen_ctrl <= '1';
 			RFr1a_ctrl <= IR_PS2(11 downto 8);	
 			RFr1e_ctrl <= '1'; -- jz if R[rn] = 0
@@ -284,15 +296,15 @@ begin
 	  when S11Delay =>
 	      stage1;
 			state <= S11;
-	  when S11 =>   Ms_ctrl <= "01";
-			Mre_ctrl <= '1'; -- read memory
+	  when S11 =>   
+	      Ms_ctrl <= "01";
 			Mwe_ctrl <= '0';		  			
-	  when S11a =>  
-			stage1b;
+	  when S11a => 
+	      stage1b; 
 			oe_ctrl <= '1'; 
-			Mre_ctrl <= '0';
 			state <= S11b;
 	  when S11b =>
+			PCinc_ctrl <= '0';
 			IR_PS2 <= IR_word;
 			state<=stage2(IR_word);
 			
@@ -302,16 +314,19 @@ begin
 			state <= S12;	
 		when s12 => 
 			Ms_ctrl <= "00";		
-			RFr1a_ctrl<= IR_word(7 downto 4);
+			RFr1a_ctrl<= IR_PS2(7 downto 4);
 			RFr1e_ctrl <= '1';
 			state <= S12a;
 		when s12a =>
-			stage1b;
+		  -- stage1b;
 			RFr1e_ctrl <='0';
 			Mwe_ctrl <='0';
+			IR_PS3 <= IR_PS2;
+			state <= S12b;
 		when s12b =>
 			RFs_ctrl <= "01";
-			RFwa_ctrl <= IR_word(11 downto 8);
+			PCinc_ctrl <= '0';
+			RFwa_ctrl <= IR_PS3(11 downto 8);
 			RFwe_ctrl <= '1';
 			IR_PS2 <= IR_word;
 			state<=stage2(IR_word);
