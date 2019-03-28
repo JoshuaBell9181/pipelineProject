5a5261c23f129efea343c98b9ec56c447fa9bd21
Joshua <31083115+JoshuaBell9181@users.noreply.github.com>
2019-03-27 23:07:16 -0300

    update display

--- SimpleCompArch2019/display.vhd
+++ SimpleCompArch2019/display.vhd
@@ -7,29 +7,29 @@ USE ieee.std_logic_1164.all;
 USE ieee.std_logic_unsigned.all;
 USE ieee.numeric_std.all;
 entity display is
-  port( CLOCK_50  : in std_logic;
-		  hexdat0      : in STD_LOGIC_VECTOR(3 downto 0);
-		  hexdat1      : in STD_LOGIC_VECTOR(3 downto 0);
-		  hexdat2      : in STD_LOGIC_VECTOR(3 downto 0);
-		  hexdat3      : in STD_LOGIC_VECTOR(3 downto 0);
-        seg0 	    : out STD_LOGIC_VECTOR(0 to 6);
-		  seg1 	    : out STD_LOGIC_VECTOR(0 to 6);
-		  seg2 	    : out STD_LOGIC_VECTOR(0 to 6);
-		  seg3 	    : out STD_LOGIC_VECTOR(0 to 6));
+  port(
+		  data      : in STD_LOGIC_VECTOR(15 downto 0);
+		  seg0 	   : out STD_LOGIC_VECTOR(0 to 6);
+		  seg1 	   : out STD_LOGIC_VECTOR(0 to 6);
+		  seg2 	   : out STD_LOGIC_VECTOR(0 to 6);
+		  seg3 	   : out STD_LOGIC_VECTOR(0 to 6));
+        
 end display;
 
 architecture display_logic_func of display is
 
-SIGNAL clk : STD_LOGIC;
-SIGNAL test: STD_LOGIC_VECTOR(3 downto 0);
+SIGNAL hexdat0      :  STD_LOGIC_VECTOR(3 downto 0);
+SIGNAL hexdat1      :  STD_LOGIC_VECTOR(3 downto 0);
+SIGNAL hexdat2      :  STD_LOGIC_VECTOR(3 downto 0);
+SIGNAL hexdat3      :  STD_LOGIC_VECTOR(3 downto 0);
 
 --On board clock 
-COMPONENT  clk_gen IS  
-	  generic( n  : integer := 25000;
-				  n1 : integer := 2000);  
-	  port( Clock : in  std_logic;
-			  c_out : out std_logic );
-END COMPONENT;
+--COMPONENT  clk_gen IS  
+--	  generic( n  : integer := 25000;
+--				  n1 : integer := 2000);  
+--	  port( Clock : in  std_logic;
+--			  c_out : out std_logic );
+--END COMPONENT;
 
 --7 segement display
 COMPONENT B_7S is
@@ -41,13 +41,16 @@ END COMPONENT;
 begin
 
 -- Setting clock
-GO: clk_gen generic map(25000,1000) Port map (CLOCK_50,clk);
+--GO: clk_gen generic map(25000,1000) Port map (CLOCK_50,clk);
 
-test <= "0000";
+hexdat0 <= data(15 downto 12); 
+hexdat1 <= data(11 downto 8); 
+hexdat2 <= data(7 downto 4); 
+hexdat3 <= data(3 downto 0); 
 
-display_seg: B_7S PORT MAP (test,seg0);
-display_seg1: B_7S PORT MAP (test,seg1);
-display_seg2: B_7S PORT MAP (test,seg2);
-display_seg3: B_7S PORT MAP (test,seg3);
+display_seg: B_7S PORT MAP  (hexdat0,seg0);
+display_seg1: B_7S PORT MAP (hexdat1,seg1);
+display_seg2: B_7S PORT MAP (hexdat2,seg2);
+display_seg3: B_7S PORT MAP (hexdat3,seg3);
 	
 end display_logic_func;
